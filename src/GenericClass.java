895e32f1c948934185a046a118c42f0d26ae2779
AbeKhan <128449118+AbeKhan@users.noreply.github.com>
2023-09-21 15:09:25 -0500

    Started the thing, now gotta understand it

---
+++ src/GenericClass.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+public class GenericClass<T>
+{
+    T type;
+    private ArrayList<T> list;
+
+    public GenericClass(T type)
+    {
+        list = new ArrayList<T>();
+    }
+
+    public 
+
+
+}
