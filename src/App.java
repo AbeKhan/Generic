8e2f65e29a939cd1bff62a6127fe74f28be25f59
AbeKhan <128449118+AbeKhan@users.noreply.github.com>
2023-09-25 15:58:28 -0500

    A little more better.

--- src/App.java
+++ src/App.java
@@ -3,9 +3,9 @@ public class App
 {
     public static void main(String[] args) throws Exception 
     {
-        /* 
+        
         GenericClass<String> test = new GenericClass<String>("s");
-
+        /* 
         System.out.println(test.isEmpty());
         String ab = "test";
         String m = "new-top";
@@ -14,8 +14,12 @@ public class App
         test.push(m);
         test.push(f);
         test.peek();
+        test.pop();
+        test.peek();
         System.out.println(test.isEmpty());
         */
+
+        
     }
     
 }
